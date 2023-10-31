9702221070197884e3ea3996b3c32a87f9450b4a
khizxr <khizharrana@gmail.com>
2023-10-31 13:47:16 +0000

    task3

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ def find_factors(n):
             factorlist.append(i)
             print(factorlist)
     return(factorlist)
+
+def primenum(num):
+    if num > 1:
+        for i in range(2, int(num/2)+1):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+                break
+        else:
+            print(num, "is a prime number")
